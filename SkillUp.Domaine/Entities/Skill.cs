41e7f58ded085d62109b2bd83b32c53cbfc1a7fd
Math Peeters <pmathieu.peeters@gmail.com>
2026-04-24 14:19:44 +0200

    feat: add Skill entity

--- SkillUp.Domaine/Entities/Skill.cs
+++ SkillUp.Domaine/Entities/Skill.cs
@@ -6,13 +6,10 @@ public class Skill
   public required string Name { get; set; }
   public string Description { get; set; } = string.Empty;
   
-<<<<<<< HEAD
+
   public Guid CategoryId { get; set; }
   public virtual Category Category { get; set; } = null!;
-=======
-  public int CategoryId { get; set; }
-  public virtual required Category Category { get; set; }
->>>>>>> 17aa230 (feat: add nav and fk to entities)
+
 
   public virtual ICollection<Evaluate> Evaluations { get; set; } = new List<Evaluate>();
   public virtual ICollection<Mentoring> Mentorings { get; set; } = new List<Mentoring>();
