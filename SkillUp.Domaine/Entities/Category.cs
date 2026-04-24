304533dfd4ce33daed27dfab8acb3b07b2427bf0
Math Peeters <pmathieu.peeters@gmail.com>
2026-04-24 14:18:14 +0200

    feat: add entity Category

---
+++ SkillUp.Domaine/Entities/Category.cs
@@ -0,0 +1,8 @@
+﻿namespace SkillUp.Domaine.Entities;
+
+public class Category
+{
+  public Guid Id { get; set; }
+  public required string Name { get; set; }
+  public string Description { get; set; } = string.Empty;
+}
