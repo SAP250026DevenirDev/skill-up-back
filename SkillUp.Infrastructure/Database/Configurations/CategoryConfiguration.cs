936af2e1588660e0b330f5e2b65a35f0af8f4945
Math Peeters <pmathieu.peeters@gmail.com>
2026-04-24 15:05:05 +0200

    feat: create Category configuration

---
+++ SkillUp.Infrastructure/Database/Configurations/CategoryConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SkillUp.Domaine.Entities;
+
+namespace SkillUp.Infrastructure.Database.Configurations;
+
+public class CategoryConfiguration : IEntityTypeConfiguration<Category>
+{
+  public void Configure(EntityTypeBuilder<Category> builder)
+  {
+    builder.HasKey(c => c.Id);
+
+    builder.Property(c => c.Name).IsRequired()
+      .HasColumnType("nvarchar");
+    builder.Property(c => c.Description)
+      .HasColumnType("nvarchar");
+    
+    
+  }
+}
