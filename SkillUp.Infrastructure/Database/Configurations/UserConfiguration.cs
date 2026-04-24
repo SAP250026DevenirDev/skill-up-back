f7ce3612442e0bb34b46b3e59e091abe1a70d4eb
Math Peeters <pmathieu.peeters@gmail.com>
2026-04-24 16:26:48 +0200

    feat: Create UserConfig

---
+++ SkillUp.Infrastructure/Database/Configurations/UserConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SkillUp.Domaine.Entities;
+
+namespace SkillUp.Infrastructure.Database.Configurations;
+
+public class UserConfiguration: IEntityTypeConfiguration<User>
+{
+  public void Configure(EntityTypeBuilder<User> builder)
+  {
+    builder.HasKey(u => u.Id);
+
+    builder.Property(u => u.FirstName)
+      .IsRequired();
+    
+    builder.Property(u => u.LastName)
+      .IsRequired();
+    
+    builder.Property(u => u.Email)
+      .IsRequired();
+    
+    //todo à finir
+  }
+}
