b4d58b80eb55382b2d4881c4f67fc91dec834b68
Math Peeters <pmathieu.peeters@gmail.com>
2026-04-24 14:26:38 +0200

    feat: create Mentoring entity

---
+++ SkillUp.Domaine/Entities/Mentoring.cs
@@ -0,0 +1,9 @@
+﻿using SkillUp.Domaine.Enums;
+
+namespace SkillUp.Domaine.Entities;
+
+public class Mentoring
+{
+  public required MentoringStatus Type { get; set; }
+  public DateTime CreatedAt { get; set; }
+}
