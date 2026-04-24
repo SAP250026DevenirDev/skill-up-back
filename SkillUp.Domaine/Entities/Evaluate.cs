cf50d0848ebcd5e36a9e717e1f89b27483e216e7
Math Peeters <pmathieu.peeters@gmail.com>
2026-04-24 14:23:37 +0200

    feat: create Evaluate entity

--- SkillUp.Domaine/Entities/Evaluate.cs
+++ SkillUp.Domaine/Entities/Evaluate.cs
@@ -4,18 +4,13 @@ public class Evaluate
 {
   public required int Level { get; set; }
   public string Comment { get; set; } = string.Empty;
-<<<<<<< HEAD
+
   
   public Guid UserId { get; set; }
   public virtual User User { get; set; } = null!;
 
   public Guid SkillId { get; set; }
-=======
-  public int UserId { get; set; }
-  public virtual User User { get; set; } = null!;
 
-  public int SkillId { get; set; }
->>>>>>> 17aa230 (feat: add nav and fk to entities)
   public virtual Skill Skill { get; set; } = null!;
 
   
