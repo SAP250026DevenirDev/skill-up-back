5db7f0eb27d74e93e1e2d6b29753b8acc5c10a99
Math Peeters <pmathieu.peeters@gmail.com>
2026-04-24 14:58:55 +0200

    feat: create DbContext

---
+++ SkillUp.Infrastructure/Database/Context/SkillUpDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SkillUp.Domaine.Entities;
+
+namespace SkillUp.Infrastructure.Database.Context;
+
+public class SkillUpDbContext(DbContextOptions<SkillUpDbContext> options) : DbContext(options)
+{
+  public DbSet<Category> Category { get; set; } = null!;
+  public DbSet<Evaluate> Evaluate { get; set; } = null!;
+  public DbSet<Mentoring> Mentoring { get; set; } = null!;
+  public DbSet<Skill> Skill { get; set; } = null!;
+  public DbSet<User> User { get; set; } = null!;
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    base.OnModelCreating(modelBuilder);
+    modelBuilder.ApplyConfigurationsFromAssembly(typeof(SkillUpDbContext).Assembly);
+  }
+}
