868360e8cf72603bac5b1c58da5b383193e7da14
Math Peeters <pmathieu.peeters@gmail.com>
2026-04-24 16:13:59 +0200

    feat: create SkillConfig

---
+++ SkillUp.Infrastructure/Database/Configurations/SkillConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SkillUp.Domaine.Entities;
+
+namespace SkillUp.Infrastructure.Database.Configurations;
+
+public class SkillConfiguration : IEntityTypeConfiguration<Skill>
+{
+  public void Configure(EntityTypeBuilder<Skill> builder)
+  {
+    builder.HasKey(s => s.Id);
+    
+    builder.Property(s => s.Name)
+      .IsRequired()
+      .HasColumnType("nvarchar");
+    
+    builder.Property(s => s.Description)
+      .HasColumnType("nvarchar");
+    
+    builder.HasOne(s => s.Category)
+      .WithMany(c => c.Skills)
+      .HasForeignKey(s => s.CategoryId)
+      .OnDelete(DeleteBehavior.Cascade);
+    
+    builder.ToTable("Skills");
+  }
+}
