24afa433c7e86cd3d2a3c509afef6e9122ccc1ea
Math Peeters <pmathieu.peeters@gmail.com>
2026-04-24 15:55:55 +0200

    feat: create MentoringConfig fix : Add id to Mentoring entity

---
+++ SkillUp.Infrastructure/Database/Configurations/MentoringConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SkillUp.Domaine.Entities;
+
+namespace SkillUp.Infrastructure.Database.Configurations;
+
+public class MentoringConfiguration : IEntityTypeConfiguration<Mentoring>
+{
+  public void Configure(EntityTypeBuilder<Mentoring> builder)
+  {
+    builder.HasKey(m => m.Id);
+    
+    builder.HasOne(m => m.Mentor)
+      .WithMany(u => u.MentoringsAsMentor)
+      .HasForeignKey(m => m.MentorId)
+      .OnDelete(DeleteBehavior.Restrict);
+    
+    builder.HasOne(m => m.Collaborator)
+      .WithMany(u => u.MentoringsAsCollaborator)
+      .HasForeignKey(m => m.CollaboratorId)
+      .OnDelete(DeleteBehavior.Restrict);
+    
+    builder.HasOne(m => m.Skill)
+      .WithMany(s => s.Mentorings)
+      .HasForeignKey(m => m.SkillId);
+  }
+}
