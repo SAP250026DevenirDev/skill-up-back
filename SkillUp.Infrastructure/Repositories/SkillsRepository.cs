7eb6933a63fafae77c17fbe23aad04aac239f844
Verly1969 <eddyverly@icloud.com>
2026-04-27 17:04:39 +0200

    création repo skill add

---
+++ SkillUp.Infrastructure/Repositories/SkillsRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using SkillUp.Core.Interfaces.Repositories;
+using SkillUp.Domaine.Entities;
+using SkillUp.Infrastructure.Database.Context;
+
+namespace SkillUp.Infrastructure.Repositories
+{
+    public class SkillsRepository : ISkillsRepository
+    {
+        private readonly SkillUpDbContext _context;
+
+        public SkillsRepository(SkillUpDbContext skillUpDbContext)
+        {
+            _context = skillUpDbContext;
+        }
+        public Skill Create(Skill skill)
+        {
+            EntityEntry<Skill> skillCreated = _context.Skills.Add(skill);
+            _context.SaveChanges();
+
+            return skillCreated.Entity;
+        }
+    }
+}
