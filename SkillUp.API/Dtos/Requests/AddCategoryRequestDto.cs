ad126440854ca71fabc3c56981fe0de0950fbc72
slaur88 <laurentsteve8@gmail.com>
2026-04-27 16:56:40 +0200

    Feat category implementation category

---
+++ SkillUp.API/Dtos/Requests/AddCategoryRequestDto.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SkillUp.API.Dtos.Requests;
+
+public class AddCategoryRequestDto
+{
+    [Required(ErrorMessage = "the name of the category is needed")]
+    public  string CategoryName { get; set; }
+    public string? CategoryDescription { get; set; }
+}
