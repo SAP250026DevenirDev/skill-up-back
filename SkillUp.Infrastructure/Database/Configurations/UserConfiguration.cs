5ed3dec19abdbca65c73fd9461d101054e1c5a24
Math Peeters <pmathieu.peeters@gmail.com>
2026-04-27 09:16:56 +0200

    feat: create UserConfig fix: set the dbsets to plural

--- SkillUp.Infrastructure/Database/Configurations/UserConfiguration.cs
+++ SkillUp.Infrastructure/Database/Configurations/UserConfiguration.cs
@@ -4,21 +4,27 @@ using SkillUp.Domaine.Entities;
 
 namespace SkillUp.Infrastructure.Database.Configurations;
 
-public class UserConfiguration: IEntityTypeConfiguration<User>
+public class UserConfiguration : IEntityTypeConfiguration<User>
 {
   public void Configure(EntityTypeBuilder<User> builder)
   {
-    builder.HasKey(u => u.Id);
-
     builder.Property(u => u.FirstName)
       .IsRequired();
-    
+
     builder.Property(u => u.LastName)
       .IsRequired();
-    
+
     builder.Property(u => u.Email)
       .IsRequired();
-    
-    //todo à finir
+
+    builder.Property(u => u.HashedPassword)
+      .IsRequired();
+
+    builder.Property(u => u.Role)
+      .IsRequired();
+
+    builder.HasKey(u => u.Id);
+    builder.Property(u => u.Email).IsRequired().HasMaxLength(150);
+    builder.HasIndex(u => u.Email).IsUnique();
   }
 }
--- SkillUp.Infrastructure/Database/Context/SkillUpDbContext.cs
+++ SkillUp.Infrastructure/Database/Context/SkillUpDbContext.cs
@@ -5,11 +5,11 @@ namespace SkillUp.Infrastructure.Database.Context;
 
 public class SkillUpDbContext(DbContextOptions<SkillUpDbContext> options) : DbContext(options)
 {
-  public DbSet<Category> Category { get; set; } = null!;
-  public DbSet<Evaluate> Evaluate { get; set; } = null!;
-  public DbSet<Mentoring> Mentoring { get; set; } = null!;
-  public DbSet<Skill> Skill { get; set; } = null!;
-  public DbSet<User> User { get; set; } = null!;
+  public DbSet<Category> Categories { get; set; } = null!;
+  public DbSet<Evaluate> Evaluates { get; set; } = null!;
+  public DbSet<Mentoring> Mentorings { get; set; } = null!;
+  public DbSet<Skill> Skills { get; set; } = null!;
+  public DbSet<User> Users { get; set; } = null!;
 
   protected override void OnModelCreating(ModelBuilder modelBuilder)
   {
