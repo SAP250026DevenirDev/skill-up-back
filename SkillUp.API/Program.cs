1405e7eb63cf64518e5ff7a9d9454e22c2aebc68
slaur88 <laurentsteve8@gmail.com>
2026-04-28 11:35:18 +0200

    (Feat) add category

--- SkillUp.API/Program.cs
+++ SkillUp.API/Program.cs
@@ -7,6 +7,8 @@ using SkillUp.Core.Interfaces.Repositories;
 using SkillUp.Infrastructure.Repositories;
 using SkillUp.Core.Services;
 
+using SkillUp.Core.Services.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add Skill
@@ -16,7 +18,11 @@ builder.Services.AddScoped<ISkillsRepository, SkillsRepository>();
 builder.Services.AddDbContext<SkillUpDbContext>(options =>
   options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+
 // Add services to the container.
+
+builder.Services.AddScoped<ICategoryRepository, CategoryRepository>();
+builder.Services.AddScoped<ICategoryService, CategoryService>();
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
