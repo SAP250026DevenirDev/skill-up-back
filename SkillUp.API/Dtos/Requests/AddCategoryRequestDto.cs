8b9361f5c28a207c4876dad79e0d3dc0ce698d64
slaur88 <laurentsteve8@gmail.com>
2026-04-28 16:13:11 +0200

    correction demandée par luca

--- SkillUp.API/Dtos/Requests/AddCategoryRequestDto.cs
+++ SkillUp.API/Dtos/Requests/AddCategoryRequestDto.cs
@@ -5,6 +5,6 @@ namespace SkillUp.API.Dtos.Requests;
 public class AddCategoryRequestDto
 {
     [Required(ErrorMessage = "the name of the category is needed")]
-    public  string CategoryName { get; set; }
+    public string CategoryName { get; set; } = null!;
     public string? CategoryDescription { get; set; }
 }
