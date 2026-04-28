08db8ce32f69085d6b0ec41ff99bf989713e5a12
Verly1969 <eddyverly@icloud.com>
2026-04-28 12:07:31 +0200

    feat: admin-add-skill finish

---
+++ SkillUp.Core/Services/SkillService.cs
@@ -0,0 +1,23 @@
+﻿using SkillUp.Core.Interfaces.Repositories;
+using SkillUp.Core.Interfaces.Services;
+using SkillUp.Domaine.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SkillUp.Core.Services
+{
+    public class SkillService : ISkillService
+    {
+        private readonly ISkillsRepository _skillsRepository;
+
+        public SkillService(ISkillsRepository skillsRepository)
+        {
+            _skillsRepository = skillsRepository;   
+        }
+        public Skill Create(Skill skill)
+        {
+            return _skillsRepository.Create(skill);
+        }
+    }
+}
