c2d5151c35b407e4f73a244b46811cbeae0fa278
slaur88 <laurentsteve8@gmail.com>
2026-04-28 09:51:59 +0200

    add services

---
+++ SkillUp.Core/Services/Data/ICategoryService.cs
@@ -0,0 +1,12 @@
+﻿using SkillUp.Domaine.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SkillUp.Core.Services.Data;
+
+public interface ICategoryService
+{
+    Task AddAsync (Category category);
+
+}
