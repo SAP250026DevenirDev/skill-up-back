e6ac7a42db4f4f2282da8274e021a329ceee2d29
slaur88 <laurentsteve8@gmail.com>
2026-04-28 14:31:48 +0200

    (feat) ajout mapper

--- SkillUp.API/Dtos/Requests/AddCategoryRequestDto.cs
+++ SkillUp.API/Dtos/Requests/AddCategoryRequestDto.cs
@@ -5,6 +5,6 @@ namespace SkillUp.API.Dtos.Requests;
 public class AddCategoryRequestDto
 {
     [Required(ErrorMessage = "the name of the category is needed")]
-    public  string CategoryName { get; set; }
-    public string? CategoryDescription { get; set; }
+    public  string Name { get; set; }
+    public string? Description { get; set; }
 }
---
+++ SkillUp.API/Mappers/CategoryMapper.cs
@@ -0,0 +1,19 @@
+﻿using SkillUp.API.Dtos.Requests;
+using SkillUp.Domaine.Entities;
+
+namespace SkillUp.API.Mappers;
+
+public static class CategoryMapper
+{
+
+    public static Category ToModel(this AddCategoryRequestDto dto)
+    {
+        return new Category()
+        {
+            Id = Guid.NewGuid(),
+            Name = dto.Name,
+            Description = dto.Description,
+        };
+    }
+}
+
