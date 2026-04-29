265bc526c9e2d05a777936ee75e2e154edeb33e7
Kévin Brogniez <kbrogniez8@gmail.com>
2026-04-29 14:01:50 +0200

    feat(category): add UpdateCategoryRequestDto

---
+++ SkillUp.API/Dtos/Requests/UpdateCategoryRequestDto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SkillUp.API.Dtos.Requests
+{
+    public class UpdateCategoryRequestDto
+    {
+        [Required(ErrorMessage = "the name of the category is needed")]
+        [MinLength(3)]
+        [MaxLength(100)]
+        public string Name { get; set; } = null!;
+
+        [MaxLength(500)]
+        public string? Description { get; set; }
+    }
+}
