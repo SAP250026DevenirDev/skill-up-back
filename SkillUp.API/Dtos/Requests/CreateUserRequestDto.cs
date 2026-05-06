6ca4eb9b3222ccd13fd9765ff9cdee1dd5f2ff21
Math Peeters <pmathieu.peeters@gmail.com>
2026-05-06 11:02:17 +0200

    feat: Adding CreateUserRequestDto.cs

---
+++ SkillUp.API/Dtos/Requests/CreateUserRequestDto.cs
@@ -0,0 +1,14 @@
+﻿using System.Text.Json.Serialization;
+using SkillUp.Domaine.Enums;
+
+namespace SkillUp.API.Dtos.Requests;
+
+public class CreateUserRequestDto
+{
+  public string FirstName { get; set; } = string.Empty;
+  public string LastName { get; set; } = string.Empty;
+  public string Email { get; set; } = string.Empty;
+  public string Password { get; set; } = string.Empty;
+  [JsonConverter(typeof(JsonStringEnumConverter))]  // conversion des entiers de l'enum en texte
+  public Roles Role { get; set; } = Roles.Collaborator; //ici, j'ajoute un champ role pour que l'admin puisse choisir le role
+}
