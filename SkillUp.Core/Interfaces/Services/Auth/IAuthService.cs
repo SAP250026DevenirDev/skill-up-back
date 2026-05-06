b4a479fa9772a967097c342efb256f63cc9fc248
Math Peeters <pmathieu.peeters@gmail.com>
2026-05-06 11:12:49 +0200

    feat: Add CreateUserByAdmin to IAuthService.cs

--- SkillUp.Core/Interfaces/Services/Auth/IAuthService.cs
+++ SkillUp.Core/Interfaces/Services/Auth/IAuthService.cs
@@ -9,5 +9,18 @@ namespace SkillUp.Core.Interfaces.Services.Auth
     {
         Task<User> LoginAsync(string email, string password);
         Task<User?> RegisterAsync(string email, string FirstName, string LastName, string Role);
+
+        #region CreateUserByAdmin
+
+        /// <summary>
+        /// Allows an administrator to manually create a new user in the system.
+        /// </summary>
+        /// <param name="user">The user entity containing basic information and the assigned role.</param>
+        /// <param name="password">The plain-text password to be hashed before storage.</param>
+        /// <returns>A task representing the asynchronous operation, containing the created user with their generated ID.</returns>
+        /// <exception cref="Exception">Thrown if the email is already in use or if the data is invalid.</exception>
+        Task<User> CreateUserByAdminAsync(User user, string password);
+
+        #endregion
     }
 }
