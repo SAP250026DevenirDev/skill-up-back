703d52d18b0e56f0ce0445bb5df3d18fa1f700c3
shrek206 <kevin.grignard.pro@gmail.com>
2026-05-08 14:36:51 +0200

    feature(US26 create method passwordChange + verif)

---
+++ SkillUp.API/Controllers/UserController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SkillUp.API.Dtos.Requests;
+using SkillUp.Core.Interfaces.Services.Data;
+using SkillUp.Domaine.Entities;
+
+namespace SkillUp.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController(IUserService _userService) : ControllerBase
+    {
+        /// <summary>
+        /// Updates the user's password after verifying the current one.
+        /// </summary>
+        /// <param name="id">The unique identifier of the user.</param>
+        /// <param name="dto">The data transfer object containing the current and new passwords.</param>
+        /// <returns>An <see cref="IActionResult"/> indicating the result of the update.</returns>
+        /// <response code="200">Password updated successfully.</response>
+        /// <response code="401">The current password is incorrect.</response>
+        /// <response code="404">User not found.</response>
+        [HttpPatch("passwordChange/{id}")]
+        [ProducesResponseType(typeof(User), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        public async Task<IActionResult> PasswordChange(Guid id, [FromBody] PasswordRequestUpdateDto dto)
+        {
+            try
+            {
+            var entity = await _userService.GetByIdAsync(id);
+            if (entity is null)
+                {
+                    return BadRequest();
+                }
+                await _userService.UpdatePassword(id, dto.CurrentPassword, dto.NewPassword);
+                return Ok();
+            }
+            catch (KeyNotFoundException ex)
+            {
+                return NotFound(ex.Message);
+            }
+            catch (AccessViolationException ex)
+            {
+                return Unauthorized(ex.Message);
+            }
+            catch (UnauthorizedAccessException ex)
+            {
+                return Unauthorized(new { error = ex.Message });
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An unexpected error occurred : {ex.Message}");
+            }
+        }
+    }
+}
---
+++ SkillUp.Core/Interfaces/Services/Data/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using SkillUp.Domaine.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SkillUp.Core.Interfaces.Services.Data
+{
+    public interface IUserService
+    {
+        Task UpdatePassword (Guid id, string currentPassword, string newPassword);
+        Task<User?> GetByIdAsync(Guid id);
+    }
+}
