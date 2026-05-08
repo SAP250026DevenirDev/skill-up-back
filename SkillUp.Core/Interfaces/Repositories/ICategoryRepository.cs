998389d7b035452f7ad9227426c9299ae55c1d7d
Kévin Brogniez <kbrogniez8@gmail.com>
2026-05-08 16:29:38 +0200

    feat(category): add GetAll and Delete methods to ICategoryRepository

--- SkillUp.Core/Interfaces/Repositories/ICategoryRepository.cs
+++ SkillUp.Core/Interfaces/Repositories/ICategoryRepository.cs
@@ -1,14 +1,17 @@
-﻿using System;
+﻿using SkillUp.Domaine.Entities;
+using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
-using SkillUp.Domaine.Entities;
 
 namespace SkillUp.Core.Interfaces.Repositories
 {
     public interface ICategoryRepository
     {
-        Task<Category?> GetByIdsAsync(Guid Id);
+        Task<List<Category>> GetAllAsync();
+        Task<Category?> GetByIdAsync(Guid Id);
         Task<Category?> GetByNameAsync(string name);
         Task<Category> AddAsync(Category category);
         Task<Category?> UpdateAsync(Guid id, Category category);
+        Task DeleteAsync(Guid id);
     }
 }
