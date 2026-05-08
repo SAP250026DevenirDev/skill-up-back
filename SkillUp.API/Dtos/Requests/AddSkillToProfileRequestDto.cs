b28ad3a4251550372441d3fc643fbd52f73e749d
slaur88 <laurentsteve8@gmail.com>
2026-05-08 14:09:47 +0200

    US15 AddSkillToProfile DTO + Mapper

---
+++ SkillUp.API/Dtos/Requests/AddSkillToProfileRequestDto.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SkillUp.API.Dtos.Requests
+{
+    public class AddSkillToProfileRequestDto
+    {
+        [Required]
+        public Guid SkillId { get; set; }
+
+        [Required]
+        [Range(1, 5, ErrorMessage = "Level must be between 1 and 5.")]
+        public int Level { get; set; }
+
+
+    }
+}
---
+++ SkillUp.API/Dtos/Responses/AddSkillToProfileResponseDto.cs
@@ -0,0 +1,12 @@
+﻿namespace SkillUp.API.Dtos.Responses
+{
+    public class AddSkillToProfileResponseDto
+    {
+        public Guid SkillId { get; set; }
+        public string? SkillName { get; set; }
+        public int Level { get; set; }
+        public string? CategoryName { get; set; }
+
+        public DateTime LastUpdate { get; set; }
+    }
+}
