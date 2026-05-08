18c01f66323ec5a26d218d73bb006af175dbab22
Kévin Brogniez <kbrogniez8@gmail.com>
2026-05-08 16:32:41 +0200

    feat(category): add GetAll and Delete methods to ICategoryService

--- SkillUp.Core/Interfaces/Services/Data/CategoryService.cs
+++ SkillUp.Core/Interfaces/Services/Data/CategoryService.cs
@@ -2,6 +2,7 @@
 using SkillUp.Core.Services.Data;
 using SkillUp.Domaine.Entities;
 using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 
 namespace SkillUp.Core.Interfaces.Services.Data;
@@ -9,12 +10,19 @@ namespace SkillUp.Core.Interfaces.Services.Data;
 public class CategoryService(ICategoryRepository _categoryRepository) : ICategoryService
 {
     /// <summary>
+    /// Récupère toutes les catégories.
+    /// </summary>
+    public async Task<List<Category>> GetAllAsync()
+    {
+        return await _categoryRepository.GetAllAsync();
+    }
+    /// <summary>
     /// Récupère une catégorie par son identifiant unique.
     /// Lance une KeyNotFoundException si aucune catégorie n'est trouvée.
     /// </summary>
-    public async Task<Category?> GetByIdsAsync(Guid id)
+    public async Task<Category?> GetByIdAsync(Guid id)
     {
-        Category? category = await _categoryRepository.GetByIdsAsync(id);
+        Category? category = await _categoryRepository.GetByIdAsync(id);
         if (category == null) throw new KeyNotFoundException();
         return category;
     }
@@ -58,5 +66,14 @@ public class CategoryService(ICategoryRepository _categoryRepository) : ICategor
             throw new ArgumentNullException(nameof(category), "Category cannot be null");
 
         return await _categoryRepository.UpdateAsync(id, category);
+    }
+
+    /// <summary>
+    /// Supprime une catégorie par son identifiant.
+    /// Lance une KeyNotFoundException si la catégorie n'est pas trouvée.
+    /// </summary>
+    public async Task DeleteAsync(Guid id)
+    {
+        await _categoryRepository.DeleteAsync(id);
     }
 }
